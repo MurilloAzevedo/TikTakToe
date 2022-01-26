5422979afe5b96ece61d883040ece0209961d711
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2022-01-26 20:21:35 -0300

    Mudança no método Board

--- src/Board.java
+++ src/Board.java
@@ -1,28 +1,37 @@
 import java.security.cert.X509Certificate;
 
 public class Board {
-    static int  LINE =  4;
-    static int COLUMN = 4;
+    final static int linha = 5;
+    final static int coluna = 5;
+    static String[][] aux = new String[linha][coluna];
+
+    static String[][] board = new String[linha][coluna];
 
     public static void Board() {
 
-        for (int i = 0; i <= LINE; i++) {
+        for (int l = 0; l < linha; l++) {
+            for (int c = 0; c < coluna; c++) {
 
-            for (int j = 0; j <= COLUMN; j++) {
+//				if(l == 1 && c ==1) {
+//					board[1][1] = "x";
+//					aux[l][c] = board[1][1];
+//					System.out.print(board[1][1]);
+//				}
 
-                if (i % 2 == 0){
-                    if (j % 2 == 0 && j > 0) {
-                        System.out.print("|  ");
-                    }
-                    else {
+                if (l % 2 == 0) {
+                    if (c % 2 == 0 && c > 0) {
+                        board[l][c] = "|";
+                        System.out.print(board[l][c]);
+                    } else if(aux[l][c] == null ){
                         System.out.print(" ");
                     }
-
-                }else if( i % 2 != 0 && i > 0  )
-                    System.out.print("- ");
+                } else if (l % 2 != 0) {
+                    board[l][c] = "- ";
+                    System.out.print(board[l][c]);
+                }
             }
-
             System.out.println();
         }
+
     }
 }
--- src/game.java
+++ src/game.java
@@ -5,14 +5,13 @@ public class game{
 
         Scanner s = new Scanner(System.in);
 
-        System.out.print("Player 1 name:");
-            
-            String p1 = s.nextLine(); //player 1 uses X
-            s.nextLine();
-
-            System.out.print("Player 2 name:");
-            String p2 = s.nextLine(); // player 2 uses O
-            s.nextLine();
+//        System.out.print("Player 1 name:");
+//        String p1 = s.nextLine(); //player 1 uses X
+//        s.nextLine();
+//
+//        System.out.print("Player 2 name:");
+//        String p2 = s.nextLine(); // player 2 uses O
+//        s.nextLine();
 
 		Board x = new Board();
         x.Board();
