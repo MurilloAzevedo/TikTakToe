f49a5853d92ea4840c047b2b323164bcd5c7891b
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2022-01-26 16:44:40 -0300

    Organizando o projeto

--- src/exit.java
+++ src/exit.java
@@ -1,7 +1,7 @@
 public class exit {
 
     public static void Exit(){
-
+        System.out.print("Thank you for playing");
         System.exit(0);
 
     }
--- src/game.java
+++ src/game.java
@@ -4,17 +4,6 @@ public class game{
     public static void Game(){
 
         Scanner s = new Scanner(System.in);
-        String[][] board = new String[4][4]; //board 3x3
-
-        board[1][1]=" "; 
-		board[1][2]=" ";
-		board[1][3]=" ";
-		board[2][1]=" ";
-		board[2][2]=" ";
-		board[2][3]=" ";
-		board[3][1]=" ";
-		board[3][2]=" ";
-		board[3][3]=" ";
 
         System.out.print("Player 1 name:");
             
@@ -25,16 +14,9 @@ public class game{
             String p2 = s.nextLine(); // player 2 uses O
             s.nextLine();
 
-            boolean gameloop = true;
+		Board x = new Board();
+        x.Board();
 
-            System.out.println();
-				System.out.println("      +---+---+---+");
-				System.out.println("      | " + board[1][1] + " | " + board[1][2] + " | " + board[1][3] + " |");
-				System.out.println("      +---+---+---+");
-				System.out.println("      | " + board[2][1] + " | " + board[2][2] + " | " + board[2][3] + " |");
-				System.out.println("      +---+---+---+");
-				System.out.println("      | " + board[3][1] + " | " + board[3][2] + " | " + board[3][3] + " |");
-				System.out.println("      +---+---+---+");
     }
 
 }
